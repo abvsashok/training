6ade5d2702fd6886e91f5a890063202ea65eab68
abvsashok <ashokchokapu@gmail.com>
2022-03-23 11:54:57 +0530

    Issue fixes

--- src/AppRouts.js
+++ src/AppRouts.js
@@ -1,20 +1,21 @@
 import React from 'react'
-import { Switch, Route, Redirect, Router,BrowserRouter } from "react-router-dom";
+import { Switch, Route, Redirect, Router, Link, BrowserRouter } from "react-router-dom";
 
 import AboutPage from './components/pages/AboutPage';
 import ContactPage from './components/pages/ContactPage';
 import HomePage from './components/pages/HomePage';
 
 function AppRouts() {
-  return (
-    <div>
-      {/* <BrowserRouter>
-                <Route exact path="home" component={HomePage} />
-                <Route exact path="about" component={AboutPage} />
-                <Route exact path="contact" component={ContactPage} />   
-       </BrowserRouter>           */}
-    </div>
-  )
+  return <BrowserRouter>
+    <Link to="/home">Home</Link> <br />
+    <Link to="/about">About</Link> <br />
+    <Link to="/careers">Careers</Link>
+    <Switch>
+      <Route exact path="/home" component={HomePage} />
+      <Route exact path="/about" component={AboutPage} />
+      <Route exact path="/contact" component={ContactPage} />
+    </Switch>
+  </BrowserRouter>
 }
 
 export default AppRouts
