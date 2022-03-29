9dafbb9cfbdccd20b595d6afae040b271ae89831
atthuluri-uday <101588502+atthuluri-uday@users.noreply.github.com>
2022-03-29 19:56:48 +0530

    Update App.js
    commit

--- src/App.js
+++ src/App.js
@@ -1,29 +1,32 @@
-import React, { createContext,useContext,useReducer,useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Buttoncomponent from './components/btncomponent/Buttoncomponent';
-import Cardcomponent from './components/cardcomponent/Cardcomponent';
-import AppRouts from './AppRouts';
-import TableComponent from './components/tablecomponent/TableComponent';
-import Header from './components/Shared/Header';
-import axios from "axios";
+import React from "react";
+import "./App.css";
+import AppRouts from "./AppRouts";
+import TableComponent from "./components/tablecomponent/TableComponent";
+import Header from "./components/Shared/Header";
+// import axios from "axios";
+import Cardcomponent from "./components/cardcomponent/Cardcomponent";
+import Buttoncomponent from "./components/btncomponent/Buttoncomponent";
 
-import { tableContextProvider } from "./context/tableContext";
+import { TableContextProvider } from "./context/tableContext";
 // import getTableDataReducer from './contextApi/GetTableData/getTableDataReducer';
 // import { initialState } from './contextApi/GetTableData/getTableInitialState';
 // export const getTableContext=createContext()
 // export const useTableContext=useContext(getTableContext)
 function App() {
-// const [tabledatastate,dispatch]=useReducer(getTableDataReducer,initialState)
+  // const [tabledatastate,dispatch]=useReducer(getTableDataReducer,initialState)
   return (
     // <getTableContext.Provider value={{tabledatastate,dispatch}}>
-    <tableContextProvider>
+    <TableContextProvider>
       <div className="">
-      <AppRouts />
+        <AppRouts />
 
-        <Cardcomponent cardheading="card heading" cardbody="card body" cardtitle="card title"></Cardcomponent>
+        <Cardcomponent
+          cardheading="card heading"
+          cardbody="card body"
+          cardtitle="card title"
+        ></Cardcomponent>
       </div>
-      </tableContextProvider>
+    </TableContextProvider>
     // </getTableContext.Provider>
   );
 }
